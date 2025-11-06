650aa6ea7980de3f96f8bc3738024c1265de36dd
Jordyn <133882087+Jordynns@users.noreply.github.com>
2025-11-06 17:32:59 +0000

    Change Docker network type from macvlan to ipvlan

--- scripts/docker-network.sh
+++ scripts/docker-network.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker network create -d macvlan \
+sudo docker network create -d ipvlan \
   --subnet=192.168.1.0/24 \
   --gateway=192.168.1.1 \
+  --ip-range=192.168.1.200/29 \
   -o parent=eth0 \
-  pihole_macvlan
+  pihole_ipvlan
