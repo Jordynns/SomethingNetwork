6a4b1c4d6c25f5a014f8dc4bc51c0cfe073486c4
Jordyn <133882087+Jordynns@users.noreply.github.com>
2025-11-06 11:43:51 +0000

    Clean up output messages in docker.sh
    Removed echo statements for cleaner output during Docker installation.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-echo "Docker / Portainer Installing..."
-
-echo "Updating and Upgrading System..."
 sudo apt update && sudo apt upgrade -y
-
-echo "Installing Certificates..."
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-
-echo "Setting up Docker Keyrings..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo "Updating System and Installing Docker..."
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-
-echo "Enabling and Starting Docker Service"
 sudo systemctl enable docker
 sudo systemctl start docker
-
-echo "Creating Portainer Volume"
 docker volume create portainer_data
-
-echo "Creating Portainer Container, running on port 9443"
 docker run -d -p 8000:8000 -p 9443:9443 \
     --name=portainer \
     --restart=always \
